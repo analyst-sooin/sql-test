cee462010d9ae2e2629dd3054c63c86a4a037355
Sooin Kim <59334939+analyst-sooin@users.noreply.github.com>
2021-11-20 20:36:06 +0900

    Create Consecutive Numbers.sql

---
+++ leetcode/Meduim/Consecutive Numbers.sql
@@ -0,0 +1,57 @@
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+id is the primary key for this table.
+ 
+
+Write an SQL query to find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+
+
+
+Answer
+
+with a as
+(select id,num,
+    lag(num) over(order by id) as prev,
+    lead(num) over(order by id) as next
+from Logs)
+
+select distinct(a.num) as ConsecutiveNums
+from a
+where a.num = a.prev and a.num = a.next
+
+
